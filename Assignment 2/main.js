6b6fff62e68d316353a8df0571b3b89f2c7ecda3
LaAriesW <laaries123@gmail.com>
2024-09-04 12:07:05 -0500

    Add assignment 2

---
+++ Assignment 2/main.js
@@ -0,0 +1,52 @@
+let personName = "Fred"
+let personAge = 42
+let isStudent = false
+
+console.log(personName + " string")
+console.log(personAge + " int")
+console.log(isStudent + " boolean")
+
+let number1 = 20
+let number2 = 50
+
+console.log("number 1 is " + number1 + " number 2 is " + number2)
+console.log("addition " + (number1 + number2))
+console.log("subtraction " + (number1 - number2))
+console.log("multiplication " + (number1 * number2))
+console.log("division " + (number1 / number2))
+
+let myString = "This is my sentence."
+
+console.log("my string is: " + myString)
+console.log("String length is " + myString.length)
+console.log("first character is " + myString.charAt(0))
+console.log("last character is " + myString.charAt(myString.length-1))
+
+let myNegative = -6
+
+console.log("my negative is " + myNegative)
+console.log("Negative square root " + Math.sqrt(myNegative))
+console.log("Negative squared " + Math.pow(myNegative, 2))
+console.log("Negative absolute value " + Math.abs(myNegative))
+
+let number3 = "40"
+let number4 = "10"
+
+console.log("first number is " + number3 + " second number is " + number4)
+console.log("first number greater than second " + (number3 > number4));
+console.log("first number less than second " + (number3 < number4))
+console.log("first number equal to second " + (number3 == number4))
+
+
+let trueVar = true;
+let falseVar = false;
+
+console.log("first boolean is " + trueVar + " second boolean is " + falseVar)
+console.log("and " + (trueVar && falseVar));
+console.log("or " + (trueVar || falseVar));
+console.log("not " + (!trueVar));
+
+firstName = "La Aries"
+lastName = "Winzerling"
+
+console.log(`hello this is ${firstName} ${lastName} greeting you.`)
